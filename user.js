cc3a66a89b1abf14556d3b67ad69310c8c808e66
aleqsunder <al.eq@outlook.com>
2024-01-14 02:58:54 +0400

    Fix to tampermonkey autoinstall

--- user.js
+++ user.js
@@ -3,8 +3,8 @@
 // @namespace    https://www.youtube.com/
 // @match        https://www.youtube.com/*
 // @version      0.0.1
-// @updateURL    https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/player.js
-// @downloadURL  https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/player.js
+// @updateURL    https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/user.js
+// @downloadURL  https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/user.js
 // @description  Allow to control shorts player
 // @author       aleqsunder
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
