d79acfa51591b7f35e58d3900d882592750d91c9
aleqsunder <al.eq@outlook.com>
2024-01-24 17:28:13 +0400

    0.0.10 (with description)
    EN:
    
    Changed:
    1. Now the sound is synchronized with the sound of the original YouTube player
    
    RU:
    
    Изменено:
    1. Теперь звук синхронизирован со звуком оригинального плеера ютьюб

--- player.user.js
+++ player.user.js
@@ -2,7 +2,7 @@
 // @name         Youtube Shorts Player
 // @namespace    https://www.youtube.com/
 // @match        https://www.youtube.com/*
-// @version      0.0.9
+// @version      0.0.10
 // @updateURL    https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/player.user.js
 // @downloadURL  https://raw.githubusercontent.com/aleqsunder/youtube-shorts-player/main/player.user.js
 // @description  Allow to control shorts player
@@ -19,19 +19,85 @@
     const BUTTON_PLAY = buttonTemplate('M8 5v14l11-7z')
     const BUTTON_PAUSE = buttonTemplate('M6 19h4V5H6v14zm8-14v14h4V5h-4z')
     const BUTTON_AUDIO = buttonTemplate('M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z')
-    const BUTTON_MUTE = buttonTemplate('M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z')
     const BUTTON_SPEED = buttonTemplate('M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z')
 
     const PLAYER_TAG_NAME = 'YTD-PLAYER'
     const PLAYER_ID = 'player'
     const STYLE_BLOCK_ID = 'yts-aleqsunder'
     const PANEL_CLASSNAME = 'yts-aleqsunder-controls'
+    const YT_PLAYER_VOLUME = 'yt-player-volume'
+    const YT_PLAYER_VOLUME_DEFAULT = 50
+    const YT_PLAYER_VOLUME_MAX = 100
+    const MONTH_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 30
 
     let videoObserver = null
     let pathname = window.location.pathname
-    let lastVolume = localStorage.getItem('aleqsunder-lastVolume')?.length > 0 ? localStorage.getItem('aleqsunder-lastVolume') : '1'
-    let lastSpeed = localStorage.getItem('aleqsunder-lastSpeed')?.length > 0 ? localStorage.getItem('aleqsunder-lastSpeed') : '3'
-    let nextVideoOnFinishPlay = localStorage.getItem('aleqsunder-nextVideoOnFinishPlay')?.length > 0 ? localStorage.getItem('aleqsunder-nextVideoOnFinishPlay') === 'true' : false
+
+    function parseStringsToObjects (item = '') {
+        if (!item || typeof item !== 'string' || item.length === 0 || item[0] !== '{') {
+            return item
+        }
+
+        item = JSON.parse(item)
+        for (let index of Object.keys(item)) {
+            item[index] = parseStringsToObjects(item[index])
+        }
+
+        return item
+    }
+
+    Storage.prototype.getObject = function (key) {
+        const value = this.getItem(key)
+        return value && parseStringsToObjects(value)
+    }
+
+    Storage.prototype.setObject = function (key, value) {
+        this.setItem(key, JSON.stringify(value))
+    }
+
+    function getVolumeStorage () {
+        const local = localStorage.getObject(YT_PLAYER_VOLUME)
+        if (local) {
+            return local
+        }
+
+        return {
+            data: {
+                volume: YT_PLAYER_VOLUME_DEFAULT,
+                muted: false
+            }
+        }
+    }
+
+    let lastVolume = getVolumeStorage()
+    let lastSpeed = localStorage.getItem('aleqsunder-lastSpeed') ? localStorage.getItem('aleqsunder-lastSpeed') : '3'
+    let nextVideoOnFinishPlay = localStorage.getItem('aleqsunder-nextVideoOnFinishPlay') ? localStorage.getItem('aleqsunder-nextVideoOnFinishPlay') === 'true' : false
+
+    function getVolume () {
+        return lastVolume?.data?.volume / YT_PLAYER_VOLUME_MAX ?? YT_PLAYER_VOLUME_DEFAULT
+    }
+
+    function setVolume (volume) {
+        const now = Date.now()
+
+        lastVolume = {
+            ...lastVolume,
+            data: {
+                volume: volume * YT_PLAYER_VOLUME_MAX ^ 0,
+                muted: false
+            },
+            creation: now,
+            expiration: now + MONTH_IN_MILLISECONDS
+        }
+
+        const convertedToYouTubeFormatVolume = {
+            ...lastVolume,
+            data: JSON.stringify(lastVolume.data)
+        }
+
+        localStorage.setObject(YT_PLAYER_VOLUME, convertedToYouTubeFormatVolume)
+        sessionStorage.setObject(YT_PLAYER_VOLUME, convertedToYouTubeFormatVolume)
+    }
 
     class ShortsControlsPanel {
         controller = null
@@ -224,8 +290,7 @@
 
         onChangeAudioRangeHandler (range) {
             this.controller.video.volume = range.value
-            lastVolume = range.value
-            localStorage.setItem('aleqsunder-lastVolume', String(range.value))
+            setVolume(range.value)
         }
 
         generateAudioRange () {
@@ -260,7 +325,7 @@
 
             audio.appendChild(pointsWrapper)
 
-            audioRange.value = lastVolume
+            audioRange.value = getVolume()
             this.onChangeAudioRangeHandler(audioRange)
 
             return audio
